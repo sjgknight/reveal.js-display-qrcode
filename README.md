2446d12d5033b8a24f222ee611329467f92c1fbd
Sebastian Joerz <sebastian.joerz@stud.h-da.de>
2024-07-05 23:12:28 +0200

    Add custom url

--- README.md
+++ README.md
@@ -10,15 +10,25 @@ Add
 ```
 { src: 'plugin/reveal.js-qrcode/qrcode.min.js'},
 { src: 'plugin/reveal.js-qrcode/index.js'}
- ```
- 
- to your `dependencies` in `Reveal.initialize`.
+```
+
+to your `dependencies` in `Reveal.initialize`.
 
 ## Usage
 
 Simply add a `div` with a class of `reveal-js-qrcode` to any of your presentation's slides. It will be automatically replaced by a QR Code pointing to the URL the presentation is hosted at.
 
 If you additionally want to display the link to the URL below the QR Code, simply also add the class `reveal-js-qrcode-display-link`.
+
+## Use a custom URL
+
+Add the following code to your `dependencies` in `Reveal.initialize`:
+
+```html
+{ src: 'plugin/reveal.js-qrcode/qrcode.min.js'}, { src:
+'plugin/reveal.js-qrcode/index.js', callback: function() {
+setCustomQRCodeUrl("https://example.com");} }
+```
 
 ## TODO
 
--- index.js
+++ index.js
@@ -1,27 +1,47 @@
-(function() {
-    //TODO include slide numbers
-    var url = window.location.href;
+(function () {
+  //TODO include slide numbers
+  var url = window.location.href;
 
-    //FIXME better sizing
-    var config = Reveal.getConfig();
-    var size = Math.min(config.height,config.width) - 200;
+  //FIXME better sizing
+  var config = Reveal.getConfig();
+  var size = Math.min(config.height, config.width) - 200;
 
-    var containers = document.querySelectorAll("div.reveal-js-qrcode");
-    containers.forEach(
-        function(e){
-            e.style.display = "flex";
-            e.style.flexDirection = "column";
-            e.style.alignItems = "center";
-            var width = e.width != null ? e.width : size;
-            var height = e.height != null ? e.height : size;
-            new QRCode(e, {text:url, width:width, height:height});
-            if(e.classList.contains("reveal-js-qrcode-display-link")){
-                var p = document.createElement("p");
-                var a = document.createElement("a");
-                a.href = url;
-                a.textContent = url;
-                p.appendChild(a);
-                e.appendChild(p);
-            }
-        });
-}());
+  var containers = document.querySelectorAll('div.reveal-js-qrcode');
+  containers.forEach(function (e) {
+    e.style.display = 'flex';
+    e.style.flexDirection = 'column';
+    e.style.alignItems = 'center';
+    var width = e.width != null ? e.width : size;
+    var height = e.height != null ? e.height : size;
+    new QRCode(e, { text: url, width: width, height: height });
+    if (e.classList.contains('reveal-js-qrcode-display-link')) {
+      var p = document.createElement('p');
+      var a = document.createElement('a');
+      a.href = url;
+      a.textContent = url;
+      p.appendChild(a);
+      e.appendChild(p);
+    }
+  });
+})();
+
+function setCustomQRCodeUrl(url) {
+  var containers = document.querySelectorAll('div.reveal-js-qrcode');
+  //FIXME better sizing
+  var config = Reveal.getConfig();
+  var size = Math.min(config.height, config.width) - 200;
+  containers.forEach(function (e) {
+    e.innerHTML = '';
+    var width = e.width != null ? e.width : size;
+    var height = e.height != null ? e.height : size;
+    new QRCode(e, { text: url, width: width, height: height });
+    if (e.classList.contains('reveal-js-qrcode-display-link')) {
+      var p = document.createElement('p');
+      var a = document.createElement('a');
+      a.href = url;
+      a.textContent = url;
+      p.appendChild(a);
+      e.appendChild(p);
+    }
+  });
+}
